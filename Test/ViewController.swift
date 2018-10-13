6fbb24171ff251581ad6a08d42d02aa38e23b2ce
桑村直弥 <kuwamuranaoya@mbp.local>
2018-10-14 01:50:32 +0900

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by 桑村直弥 on 2018/10/14.
+//  Copyright © 2018年 noy4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
